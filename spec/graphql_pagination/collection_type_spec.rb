fd2cf6d4666540c474cbae1c0b9e1ac1b485abf8
Adam Pohorecki <adam@pohorecki.pl>
2021-07-08 12:34:52 +0200

    Fix caching of collection types
    The collection type can be customized with a metadata_type argument. If
    one chooses to use different metadata types for the same GQL type, then
    the first type will be cached and the custom metadata type will be
    ignored.

--- spec/graphql_pagination/collection_type_spec.rb
+++ spec/graphql_pagination/collection_type_spec.rb
@@ -11,5 +11,26 @@ RSpec.describe GraphqlPagination::CollectionType do
     it do
       expect(collection_type.fields.keys).to match_array(%w[collection metadata])
     end
+
+    context "with custom metadata type" do
+      let(:collection_type) { type.collection_type }
+      let(:custom_collection_type) { type.collection_type(metadata_type: metadata_type) }
+
+      let(:metadata_type) do
+        Class.new(GraphqlPagination::CollectionMetadataType) do
+          graphql_name 'CustomCollectionMetadataType'
+          field :foo, String, null: true
+        end
+      end
+
+      it "returns an appropriate collection type based on metadata_type argument" do
+        expect(collection_type.fields['metadata'].type.of_type.fields.keys).not_to include('foo')
+        expect(custom_collection_type.fields['metadata'].type.of_type.fields.keys).to include('foo')
+      end
+
+      it "caches the type for future use" do
+        expect(custom_collection_type).to be(type.collection_type(metadata_type: metadata_type))
+      end
+    end
   end
 end
