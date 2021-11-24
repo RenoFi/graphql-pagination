b18b157559b4712794ea51661d1962512ed4411c
Francisco Hernandez <lagcisco@lagcisco.com>
2021-11-24 11:02:37 -0600

    union support

--- lib/graphql_pagination.rb
+++ lib/graphql_pagination.rb
@@ -9,3 +9,4 @@ require 'graphql_pagination/collection_type'
 require 'graphql_pagination/collection_metadata_type'
 
 GraphQL::Schema::Object.extend GraphqlPagination::CollectionType
+GraphQL::Schema::Union.extend GraphqlPagination::CollectionType
