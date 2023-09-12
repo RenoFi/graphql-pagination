574e670f8b0091a5578c23f5056dbfbaf037a622
Brandon Case <brcase@usertesting.com>
2023-09-12 19:01:42 -0400

    Adding Description to the arguments and fields
    Some CI tools are set to require descriptions on fields and arguments
    
    https://github.com/RenoFi/graphql-pagination/issues/148
    
    Signed-off-by: Brandon Case <brcase@usertesting.com>

--- lib/graphql_pagination/collection_metadata_type.rb
+++ lib/graphql_pagination/collection_metadata_type.rb
@@ -1,8 +1,8 @@
 module GraphqlPagination
   class CollectionMetadataType < GraphQL::Schema::Object
-    field :current_page, Integer, null: false
-    field :limit_value, Integer, null: false
-    field :total_count, Integer, null: false
-    field :total_pages, Integer, null: false
+    field :current_page, Integer, null: false, description: "Current Page of loaded data"
+    field :limit_value, Integer, null: false, description: "The number of items per page"
+    field :total_count, Integer, null: false, description: "The total number of items to be paginated"
+    field :total_pages, Integer, null: false, description: "The total number of pages in the pagination"
   end
 end
