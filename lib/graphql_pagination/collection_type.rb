25c02146c7f9200675880601831dfeef09a8ab62
Brandon Case <brcase@usertesting.com>
2023-09-13 11:34:51 -0400

    fixed error using describe instead of description
    Signed-off-by: Brandon Case <brcase@usertesting.com>

--- lib/graphql_pagination/collection_type.rb
+++ lib/graphql_pagination/collection_type.rb
@@ -14,8 +14,8 @@ module GraphqlPagination
 
         Class.new(collection_base) do
           graphql_name type_name
-          field :collection, [source_type], null: false, describe: "A collection of paginated #{graphql_name}"
-          field :metadata, metadata_type, null: false, describe: "Pagination Metadata for navigating the Pagination"
+          field :collection, [source_type], null: false, description: "A collection of paginated #{graphql_name}"
+          field :metadata, metadata_type, null: false, description: "Pagination Metadata for navigating the Pagination"
 
           def collection
             object
