3f581ea60cd7d9c4d9961c51a22b03a73d148567
DP <git@dakotahpena.dev>
2023-09-15 14:20:02 -0700

    Added specs for CollectionMetadataType descriptions

--- spec/graphql_pagination/collection_metadata_type_spec.rb
+++ spec/graphql_pagination/collection_metadata_type_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe GraphqlPagination::CollectionMetadataType do
   describe '.fields' do
-    it do
+    it 'has expected fields' do
       expect(described_class.fields.keys).to match_array(%w[currentPage limitValue totalCount totalPages])
+    end
+
+    it 'has descriptions on fields' do
+      described_class.fields.each do |key, value|
+        expect(described_class.fields[key].description).to be_present
+      end
     end
   end
 end
