07ec644f9ab5380a4b4a268b063169c4ba039e07
DP <git@dakotahpena.dev>
2023-09-18 09:10:20 -0700

    Add description field to CollectionMetadataType
    * Added corresponding spec

--- lib/graphql_pagination/collection_metadata_type.rb
+++ lib/graphql_pagination/collection_metadata_type.rb
@@ -1,5 +1,6 @@
 module GraphqlPagination
   class CollectionMetadataType < GraphQL::Schema::Object
+    description "Type for CollectionMetadataType"
     field :current_page, Integer, null: false, description: "Current Page of loaded data"
     field :limit_value, Integer, null: false, description: "The number of items per page"
     field :total_count, Integer, null: false, description: "The total number of items to be paginated"
