b22b8d4fd24ea9742cce1cf5000c20e623e4139a
Krzysztof Knapik <knapo@knapo.net>
2026-01-27 12:35:59 +0100

    rubocop

--- lib/graphql_pagination/paginated_field.rb
+++ lib/graphql_pagination/paginated_field.rb
@@ -1,10 +1,10 @@
 module GraphqlPagination
   module PaginatedField
-    def paginated_field(name, type, **kwargs, &block)
-      field name, type, **kwargs do
+    def paginated_field(name, type, **, &)
+      field(name, type, **) do
         argument :page, Integer, required: false
         argument :per, Integer, required: false
-        instance_eval(&block) if block_given?
+        instance_eval(&) if block_given?
       end
     end
   end
