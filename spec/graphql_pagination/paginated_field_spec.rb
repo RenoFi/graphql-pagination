eabfebd547d932ee5be5dcb46983f5f14651763e
Krzysztof Knapik <knapo@knapo.net>
2026-04-08 16:24:08 +0200

    lint

--- spec/graphql_pagination/paginated_field_spec.rb
+++ spec/graphql_pagination/paginated_field_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe GraphqlPagination::PaginatedField do
-  describe '.paginated_field' do
+  describe ".paginated_field" do
     let(:query_type) do
       Class.new(GraphQL::Schema::Object) do
-        graphql_name 'TestQuery'
+        graphql_name "TestQuery"
 
         paginated_field :fruits, FruitType.collection_type, null: true
 
@@ -28,39 +28,39 @@ RSpec.describe GraphqlPagination::PaginatedField do
       end
     end
 
-    it 'defines field with pagination arguments' do
-      field = query_type.fields['fruits']
+    it "defines field with pagination arguments" do
+      field = query_type.fields["fruits"]
       expect(field).not_to be_nil
-      expect(field.arguments.keys).to include('page', 'per')
+      expect(field.arguments.keys).to include("page", "per")
     end
 
-    it 'page argument is optional' do
-      field = query_type.fields['fruits']
-      expect(field.arguments['page'].type.non_null?).to be false
+    it "page argument is optional" do
+      field = query_type.fields["fruits"]
+      expect(field.arguments["page"].type.non_null?).to be false
     end
 
-    it 'per argument is optional' do
-      field = query_type.fields['fruits']
-      expect(field.arguments['per'].type.non_null?).to be false
+    it "per argument is optional" do
+      field = query_type.fields["fruits"]
+      expect(field.arguments["per"].type.non_null?).to be false
     end
 
-    context 'with additional arguments in block' do
-      it 'includes additional arguments' do
-        field = query_type.fields['vegetables']
-        expect(field.arguments.keys).to include('page', 'per', 'where', 'order')
+    context "with additional arguments in block" do
+      it "includes additional arguments" do
+        field = query_type.fields["vegetables"]
+        expect(field.arguments.keys).to include("page", "per", "where", "order")
       end
 
-      it 'additional arguments are optional' do
-        field = query_type.fields['vegetables']
-        expect(field.arguments['where'].type.non_null?).to be false
-        expect(field.arguments['order'].type.non_null?).to be false
+      it "additional arguments are optional" do
+        field = query_type.fields["vegetables"]
+        expect(field.arguments["where"].type.non_null?).to be false
+        expect(field.arguments["order"].type.non_null?).to be false
       end
     end
 
-    context 'when used in queries' do
-      subject(:result) { schema.execute(query).to_h['data']['result'] }
+    context "when used in queries" do
+      subject(:result) { schema.execute(query).to_h["data"]["result"] }
 
-      context 'with page and per arguments' do
+      context "with page and per arguments" do
         let(:query) do
           %|{
             result: fruits(page: 2, per: 2) {
@@ -78,16 +78,16 @@ RSpec.describe GraphqlPagination::PaginatedField do
           }|
         end
 
-        it 'executes query successfully' do
-          expect(result['collection'].size).to eq(2)
-          expect(result['metadata']['currentPage']).to eq(2)
-          expect(result['metadata']['limitValue']).to eq(2)
+        it "executes query successfully" do
+          expect(result["collection"].size).to eq(2)
+          expect(result["metadata"]["currentPage"]).to eq(2)
+          expect(result["metadata"]["limitValue"]).to eq(2)
         end
       end
 
-      context 'without page and per arguments' do
+      context "without page and per arguments" do
         let(:query) do
-          %|{
+          %({
             result: fruits {
               collection {
                 id
@@ -100,16 +100,16 @@ RSpec.describe GraphqlPagination::PaginatedField do
                 currentPage
               }
             }
-          }|
+          })
         end
 
-        it 'executes query successfully' do
-          expect(result['collection'].size).to eq(11)
-          expect(result['metadata']['currentPage']).to eq(1)
+        it "executes query successfully" do
+          expect(result["collection"].size).to eq(11)
+          expect(result["metadata"]["currentPage"]).to eq(1)
         end
       end
 
-      context 'with additional arguments' do
+      context "with additional arguments" do
         let(:query) do
           %|{
             result: vegetables(page: 1, per: 5, where: "test") {
@@ -124,9 +124,9 @@ RSpec.describe GraphqlPagination::PaginatedField do
           }|
         end
 
-        it 'executes query successfully with additional arguments' do
-          expect(result['collection'].size).to eq(5)
-          expect(result['metadata']['totalCount']).to eq(11)
+        it "executes query successfully with additional arguments" do
+          expect(result["collection"].size).to eq(5)
+          expect(result["metadata"]["totalCount"]).to eq(11)
         end
       end
     end
