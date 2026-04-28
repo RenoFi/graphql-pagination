669b824aad04e2434a48e962be44db0ecacb2e23
Adam Pohorecki <adam@pohorecki.pl>
2026-04-28 11:13:23 +0200

    Mark CollectionMetadataType @shareable under Federation 2 (#253)

--- lib/graphql_pagination/collection_metadata_type.rb
+++ lib/graphql_pagination/collection_metadata_type.rb
@@ -1,5 +1,12 @@
 module GraphqlPagination
   class CollectionMetadataType < GraphQL::Schema::Object
+    if defined?(ApolloFederation::Object) && defined?(Reconf) &&
+        Gem::Version.new(Reconf.fetch("renoql.federation_version") { "1.0" }.to_s) >= Gem::Version.new("2.0.0")
+      include ApolloFederation::Object
+
+      shareable
+    end
+
     description "Type for CollectionMetadataType"
     field :current_page, Integer, null: false, description: "Current Page of loaded data"
     field :limit_value, Integer, null: false, description: "The number of items per page"
